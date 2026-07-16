0b926396ea4eaac579a6809ec47f564874f545f7
Panangipalli kalyani lahari <trendshoney@gmail.com>
2026-07-16 08:02:45 +0000

    osa lab program 1

---
+++ exercise1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    printf("Current Directory\n");
+    system("pwd");
+
+    printf("\nFiles\n");
+    system("ls -1");
+
+    printf("\nCreating Directory\n");
+    system("mkdir Demo");
+
+    printf("\nListing Files Again\n");
+    system("ls");
+
+    printf("\nRemoving Directory\n");
+    system("rmdir Demo");
+
+    return 0;
+}
